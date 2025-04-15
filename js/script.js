5550664d24b11be13d75908271a6b8e9e2595c34
JHS96 <jschaper@hotmail.co.za>
2025-04-15 13:21:57 +0200

    Video modal

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Mobile Menu
   const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
   const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
 
   toggleButton.addEventListener('click', function () {
     mobileMenu.classList.toggle('active');
   });
+
+  // Video Modal
+  const modal = document.getElementById('videoModal');
+  const videoButton = document.querySelector('.preview__video-button');
+  const closeButton = document.querySelector('.modal__close-button');
+  const videoPlayer = document.getElementById('videoPlayer');
+
+  // Open modal when play videoButton is clicked
+  videoButton.addEventListener('click', function () {
+    // Show modal
+    modal.style.display = 'block';
+
+    // Replace the source attribte with the video URL
+    videoPlayer.src = 'https://www.youtube.com/embed/8sXRyHI3bLw';
+
+    // Close modal on close button click
+    closeButton.addEventListener('click', function () {
+      modal.style.display = 'none';
+      videoPlayer.src = '';
+    });
+
+    // lose modal on outer/background click
+    window.addEventListener('click', function (event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+        videoPlayer.src = '';
+      }
+    });
+  });
 });
 
+// Navigation background on scroll
 window.addEventListener('scroll', function () {
   const navbar = document.querySelector('.navbar');
 
